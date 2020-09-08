4f0022ca3aa7803faf8901cf469cc26395163114
anminam <minamhunmin@gmail.com>
2020-09-09 08:09:08 +0900

    add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQvZpGTIfgNerJ2tit4soxqrOq-SlZOhg",
+  authDomain: "anmazon.firebaseapp.com",
+  databaseURL: "https://anmazon.firebaseio.com",
+  projectId: "anmazon",
+  storageBucket: "anmazon.appspot.com",
+  messagingSenderId: "358474395549",
+  appId: "1:358474395549:web:0cb2c1599072897e92c974",
+  measurementId: "G-LN4SBJV4SP",
+};
