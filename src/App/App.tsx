187d1be78082ef4bf974213461f8343f7dd56569
anminam <minamhunmin@gmail.com>
2020-09-09 08:57:21 +0900

    udpate other

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "App/App.scss";
+import Header from "Components/Header/Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Anmazon</h1>
+      <Header />
     </div>
   );
 }
