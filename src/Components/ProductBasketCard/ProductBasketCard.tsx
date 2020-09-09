bf58da405468a397d31286384934d03f7ae9df26
minamAn <minamhunmin@gmail.com>
2020-09-09 18:35:03 +0900

    update card

--- src/Components/ProductBasketCard/ProductBasketCard.tsx
+++ src/Components/ProductBasketCard/ProductBasketCard.tsx
@@ -2,24 +2,19 @@ import React from "react";
 import "./ProductBasketCard.scss";
 import { IProduct } from "interfaces/IProduct";
 import { useDispatch } from "react-redux";
-import { addToBasket } from "core/data/actions";
+import { removeFromBasket } from "core/data/actions";
 
 const ProductBasketCard = ({ title, price, img, rating, id }: IProduct) => {
   const dispatch = useDispatch();
 
   const handleButtonClick = () => {
-    dispatch(
-      addToBasket({
-        title,
-        price,
-        img,
-        rating,
-        id,
-      })
-    );
+    dispatch(removeFromBasket(id));
   };
   return (
     <div className="productbasketcard">
+      <div className="productbasketcard__image">
+        <img src={img} alt={title} />
+      </div>
       <div className="productbasketcard__info">
         <div className="productbasketcard__title">{title}</div>
         <div className="productbasketcard__price">
@@ -33,13 +28,9 @@ const ProductBasketCard = ({ title, price, img, rating, id }: IProduct) => {
             </span>
           ))}
         </div>
-        <div className="productbasketcard__rating"></div>
-      </div>
-      <div className="productbasketcard__image">
-        <img src={img} alt={title} />
-      </div>
-      <div className="productbasketcard__footer">
-        <button onClick={handleButtonClick}>Add to Cart</button>
+        <div className="productbasketcard__footer">
+          <button onClick={handleButtonClick}>Remove from Basket</button>
+        </div>
       </div>
     </div>
   );
