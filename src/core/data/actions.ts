7452a0ef7c0e518020cf66f93090e49f54f0b430
anminam <minamhunmin@gmail.com>
2020-09-10 11:21:02 +0900

    update core data

--- src/core/data/actions.ts
+++ src/core/data/actions.ts
@@ -1,5 +1,12 @@
 import { IProduct } from "interfaces/IProduct";
 
+export const SET_USER = "SET_USER" as const;
+
+export const setUser = (user: firebase.User | null) => ({
+  type: SET_USER,
+  payload: user,
+});
+
 export const ADD_TO_BASKET = "ADD_TO_BASKET" as const;
 export const REMOVE_FROM_BASKET = "REMOVE_FROM_BASKET" as const;
 
@@ -13,5 +20,6 @@ export const removeFromBasket = (id: string) => ({
 });
 
 export type TDataActions =
+  | ReturnType<typeof setUser>
   | ReturnType<typeof addToBasket>
   | ReturnType<typeof removeFromBasket>;
--- src/core/data/reducer.ts
+++ src/core/data/reducer.ts
@@ -1,14 +1,20 @@
 import { IProduct } from "interfaces/IProduct";
-import { TDataActions, ADD_TO_BASKET, REMOVE_FROM_BASKET } from "./actions";
-
-const initState = {
-  baskets: [],
-};
+import {
+  TDataActions,
+  ADD_TO_BASKET,
+  REMOVE_FROM_BASKET,
+  SET_USER,
+} from "./actions";
 
 interface IData {
+  user: firebase.User | null;
   baskets: IProduct[];
 }
 
+const initState = {
+  user: null,
+  baskets: [],
+};
 const reducer = (state: IData = initState, action: TDataActions): IData => {
   switch (action.type) {
     case ADD_TO_BASKET:
@@ -22,6 +28,11 @@ const reducer = (state: IData = initState, action: TDataActions): IData => {
         baskets: [
           ...state.baskets.filter((item) => item.id !== action.payload),
         ],
+      };
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload,
       };
     default:
       return state;
