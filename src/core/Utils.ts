ac6d52dddc8764a2a8bbc94f7af3784542341ed7
minamAn <minamhunmin@gmail.com>
2020-09-12 00:34:29 +0900

    update utils

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -13,6 +13,22 @@ const Utils = {
   getBasketTotal: (basket: IProduct[]): number => {
     return basket.reduce<number>((pre, curr) => pre + curr.price, 0);
   },
+  formatDate: (date: number) => {
+    const d = new Date(date * 1000);
+    const yyyy = d.getFullYear();
+    const mm = (d.getMonth() + 1).toString().padStart(2, "0");
+    const dd = (d.getDate() + 1).toString().padStart(2, "0");
+
+    return `${yyyy}-${mm}-${dd}`;
+  },
+  formatPrice: (price: number) => {
+    function numberWithCommas(x: number) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    price = price / 100;
+
+    return numberWithCommas(price);
+  },
 };
 
 export { Utils };
