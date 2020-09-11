9b68d93237853d56df13c2aa129d84f908396d9b
anminam <minamhunmin@gmail.com>
2020-09-11 14:26:50 +0900

    add db

---
+++ src/core/Db.ts
@@ -0,0 +1,23 @@
+import { db as _ } from "firebaseAnmazon";
+import { PaymentIntent } from "@stripe/stripe-js";
+import { IProduct } from "interfaces/IProduct";
+
+const db = {
+  setBasket: (
+    user: firebase.User,
+    paymentIntent: PaymentIntent,
+    basket: IProduct[]
+  ) => {
+    _.collection("users")
+      .doc(user.uid)
+      .collection("orders")
+      .doc(paymentIntent.id)
+      .set({
+        basket,
+        amount: paymentIntent.amount,
+        created: paymentIntent.created,
+      });
+  },
+};
+
+export { db };
