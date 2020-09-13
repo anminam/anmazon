d391d073ad227976f75657907b03f8825547b49f
minamAn <minamhunmin@gmail.com>
2020-09-13 17:56:52 +0900

    add search

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search as SearchIcon } from "@material-ui/icons";
 
 const Search = () => {
+  const [keyword, setKeyword] = useState<string>("");
   return (
     <div className="search">
-      <input type="text" className="search__input" />
+      <input
+        type="text"
+        className="search__input"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <SearchIcon />
     </div>
   );
